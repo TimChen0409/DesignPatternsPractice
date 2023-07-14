f05cd2ddde490927b9bfc97106485d0482323b22
Tim <59609847+TimChen0409@users.noreply.github.com>
2023-07-14 16:31:46 +0800

    add adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Fahrenheit(87.8);
+            ICelsius FahrenheitToCelsiusAdapter = new FahrenheitToCelsiusAdapter(f);
+            Console.WriteLine("Output攝氏溫度：" + FahrenheitToCelsiusAdapter.GetCTemperature());
+        }
+    }
+
+    public interface ICelsius
+    {
+        double GetCTemperature();
+    }
+
+    class FahrenheitToCelsiusAdapter : ICelsius
+    {
+        private readonly Fahrenheit fahrenheit;
+
+        public FahrenheitToCelsiusAdapter(Fahrenheit fahrenheit)
+        {
+            this.fahrenheit = fahrenheit;
+        }
+
+        public double GetCTemperature()
+        {
+            Console.WriteLine("Input華氏溫度：" + fahrenheit.GetFTemperature());
+            return (fahrenheit.GetFTemperature() - 32) * 5 / 9;
+        }
+    }
+
+    class Fahrenheit
+    {
+        private readonly double temperature;
+
+        public Fahrenheit(double temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public double GetFTemperature()
+        {
+            return temperature;
+        }
+    }
+}
